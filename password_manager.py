613f643cc0c3e854df3fa044c8e50e07476a9552
Roy Contreras Benitez <roy7474@gmail.com>
2023-07-31 19:42:47 -0400

    a function to encryp the passwords have been created

--- password_manager.py
+++ password_manager.py
@@ -1,9 +1,20 @@
+from cryptography.fernet import Fernet
+
 master_pwd = input('What is the master password? ')
+
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key',  'wb') as key_file:
+        key_file.write(key)
+
+write_key()
 
 def view():
     with open('password.txt', 'r') as f:
         for line in f.readlines():
-            print(line.rstrip())
+            data = line.rstrip()
+            user, passw = data.split('|')
+            print('User:', user, '| Password', passw)
 
 def add():
     name = input('Account name: ')
