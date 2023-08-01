d81eebffffd78ac9c1b3856704f7d6c080b545db
Roy Contreras Benitez <roy7474@gmail.com>
2023-07-31 20:19:07 -0400

    password encryptation and decryptation has been added

--- password_manager.py
+++ password_manager.py
@@ -1,27 +1,34 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input('What is the master password? ')
-
+'''
 def write_key():
     key = Fernet.generate_key()
     with open('key.key',  'wb') as key_file:
-        key_file.write(key)
+        key_file.write(key)'''
 
-write_key()
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input('What is the master password? ')
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
     with open('password.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split('|')
-            print('User:', user, '| Password', passw)
+            print('User:', user, '| Password:', fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input('Account name: ')
     pwd = input('Password: ')
 
     with open('password.txt', 'a') as f:
-        f.write(name + '|' + pwd + '\n')
+        f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + '\n')
 
 while True:
     mode = input('Would you like to add a new password or vew existing ones (view / add)? Press Q to quit ')
